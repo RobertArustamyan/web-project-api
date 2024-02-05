deb67c0bb031686c5a647fea2b2c832e39716c3c
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-02-05 21:02:21 +0400

    Added cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # app.py
 from flask import Flask, request, jsonify
 from getting_json_req import MakingJson
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search', methods=['GET'])
 def search():
